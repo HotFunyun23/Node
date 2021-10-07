9a4e54b6a02b08fcfe954046af778839251977d9
unknown <cschaden22@kentdenver.org>
2021-10-07 12:25:58 -0600

    the scrum lord has added code

--- src/Node.java
+++ src/Node.java
@@ -1,8 +1,61 @@
-public class Node {
-    private Object object;
+public class Node<T> {
+    private T data;
+    private Node parent;
+    private Node child;
 
-    public void set(Object object) { this.object = object; }
-    public Object get() { return object; }
+    public Node(){
+    }
+
+    public Node(T data){
+        this.data = data;
+    }
+
+    public Node(T data, Node parent, Node child){
+        this.data = data;
+        this.parent = parent;
+        this.child = child;
+    }
+
+    public void setData(T data){
+        this.data = data;
+    }
+
+    public void setParent(Node parent){
+        this.parent = parent;
+    }
+
+    public void setChild(Node child){
+        this.child = child;
+    }
+
+    public void makeChild(Node parent){
+        parent.child = this;
+    }
+
+    public void makeParent(Node child){
+        child.parent = this;
+    }
+
+    public T getData(){
+        return data;
+    }
+
+    public Node getParent(){
+        return parent;
+    }
+
+    public Node getChild(){
+        return child;
+    }
+
+    public String toString(){
+        return "Node: "+data.toString();
+    }
+
+    public boolean equals(Node userNode) {
+        return this.data.equals(userNode.getData());
+    }
+
+
 
 }
- 
